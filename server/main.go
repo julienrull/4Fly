31e9f2fb53be5a4cf3894c04a879c15e4e44e2d8
julienrull <julien.rull38@gmail.com>
2024-03-24 13:21:35 +0100

    Files cleaned

--- server/main.go
+++ server/main.go
@@ -12,7 +12,9 @@ import "net/http"
 
 type FSHandler = func(w http.ResponseWriter, r *http.Request) (doDefaultFileServe bool)
 
+
 func CustomFileServer(root http.FileSystem) http.Handler {
+    EXE := "../4Fly"
 	fs := http.FileServer(root)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//make sure the url path starts with /
@@ -32,14 +34,14 @@ func CustomFileServer(root http.FileSystem) http.Handler {
                 // check extension
                 if ext == ".m3u8" {
                     // call encoder
-                    cmd := exec.Command("./encoder", "./test.mp4", "-entity:m3u8", "-time:3.0")
+                    cmd := exec.Command(EXE, "./test.mp4", "-entity:m3u8", "-time:3.0")
                     err := cmd.Run()
                     fmt.Println(ext)
                     if err != nil {
                         log.Fatal(err)
                     }      
                 }else if ext == ".mp4" {
-                    cmd := exec.Command("./encoder", "./test.mp4", "-entity:init", "-time:3.0")
+                    cmd := exec.Command(EXE, "./test.mp4", "-entity:init", "-time:3.0")
                     err := cmd.Run()
                     fmt.Println(ext)
                     if err != nil {
@@ -49,7 +51,7 @@ func CustomFileServer(root http.FileSystem) http.Handler {
                     re := regexp.MustCompile("[0-9]+")
                     res := re.FindAllString(file_name, -1)
                     value, _ := strconv.Atoi(res[0])
-                    cmd := exec.Command("./encoder", "./test.mp4", fmt.Sprintf("-entity:%d", value), "-time:3.0")
+                    cmd := exec.Command(EXE, "./test.mp4", fmt.Sprintf("-entity:%d", value), "-time:3.0")
                     err := cmd.Run()
                     if err != nil {
                         log.Fatal(err)
